881cd8d583d8a58a5eb0062067ad760b9f7dfdf2
Beatriz de Brito <bedebrito.web@gmail.com>
2023-03-07 14:43:09 -0300

    modify app.get method to serve a html file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let app = express();
+let absolutePath = __dirname + "/views/index.html";
 console.log("Hello World");
-app.get("/", (req, res) => {res.send("Hello Express")});
+app.get("/", (req, res) => {res.sendFile(absolutePath)});
 
 
 
