b996c1cfd7f072a35809319d5dcda59e48a6412a
Beatriz de Brito <bedebrito.web@gmail.com>
2023-03-13 12:42:52 -0300

    use app.get to serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -3,8 +3,12 @@ let app = express();
 
 let indexPath = __dirname + "/views/index.html";
 let publicPath = __dirname + "/public";
+let message = {
+    "message": "Hello json"
+}
 
 app.get("/", (req, res) => { res.sendFile(indexPath) });
+app.get("/json", (req, res) => {res.json(message)});
 app.use("/public", express.static(publicPath));
 
 
