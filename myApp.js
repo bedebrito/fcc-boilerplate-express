57a37b19bb37a7f40530c296c85f057fec3e0c56
Beatriz de Brito <bedebrito.web@gmail.com>
2023-03-13 13:51:37 -0300

    use .env file

--- myApp.js
+++ myApp.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
 let indexPath = __dirname + "/views/index.html";
 let publicPath = __dirname + "/public";
-let message = {
-    "message": "Hello json"
-}
 
 app.get("/", (req, res) => { res.sendFile(indexPath) });
-app.get("/json", (req, res) => {res.json(message)});
+app.get("/json", (req, res) => {
+    process.env.MESSAGE_STYLE==="uppercase" ? 
+    res.json({"message": "HELLO JSON"}) : 
+    res.json({"message": "Hello json"})
+});
 app.use("/public", express.static(publicPath));
 
 
